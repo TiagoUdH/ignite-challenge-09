6a4c9fffb635738da119c9570de6e6feb92d1934
Tiago Fernandes <tiagoferprojects@gmail.com>
2024-02-27 21:04:59 -0300

    Implementing user session

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_socketio import SocketIO, emit
 
+import uuid
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
 
 socketio = SocketIO(app)
 
+users_amount = 0
 
 @app.route('/')
 def index():
@@ -12,15 +16,24 @@ def index():
 
 @socketio.on('message')
 def handle_message(msg):
-    emit('message', msg, broadcast=True)
+    message = f'Usuário {session['user_number']}: {msg}'
+    emit('message', message, broadcast=True)
     
 @socketio.on('connect')
 def handle_connect():
-    emit('message', 'Usuário está online', broadcast=True)
+    user_id = str(uuid.uuid4())
+    session['user_id'] = user_id
+    
+    global users_amount
+    users_amount += 1
+    
+    session['user_number'] = users_amount
+    
+    emit('message', f'Usuário {users_amount} está online', broadcast=True)
     
 @socketio.on('disconnect')
 def handle_connect():
-    emit('message', 'Usuário está offline', broadcast=True)
+    emit('message', f'Usuário {session['user_number']} está offline', broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
